0e8c01d729d92918db8bb43662704a9d8d022ce3
gabrielmulleer <emaildegabrielm@gmail.com>
2024-08-15 14:23:56 -0300

    feat: mount the tictactoe board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SquareComponent, BoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-tictactoe';
